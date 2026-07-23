ec05250373b36f0886430cd1c43e79dd6892fce3
Prajjwal Puri <prajjwalpuri04@gmail.com>
2026-07-23 10:47:22 +0530

    Adding programming question

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
-import com.sun.jdi.IntegerValue;
+/*
+   Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+Example 1:Input: s = "(()"Output: 2Explanation: The longest valid parentheses substring is "()".
+Example 2:Input: s = ")()())"Output: 4Explanation: The longest valid parentheses substring is "()()".
+Example 3:Input: s = ""Output: 0
+Constraints:
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.
+*/
 
 import java.util.Scanner;
 import java.util.Stack;
